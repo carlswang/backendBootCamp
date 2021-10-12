225b2b7882be12b8d4dace89ae29c43bdbd1b7b2
Wei Wang <wei.wang2thoughtworks.com@Weis-MacBook-Pro.local>
2021-10-12 13:09:44 +0800

    [wangwei]feat:finished a test that test find task by id is correct

--- src/test/java/com/example/backendbootcamp/controller/TaskControllerTest.java
+++ src/test/java/com/example/backendbootcamp/controller/TaskControllerTest.java
@@ -3,10 +3,15 @@ package com.example.backendbootcamp.controller;
 import com.example.backendbootcamp.entity.Task;
 import com.example.backendbootcamp.service.TaskService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(TaskController.class)
 public class TaskControllerTest {
@@ -26,5 +31,17 @@ public class TaskControllerTest {
                 .name("study")
                 .describe("Study is very important to everyone")
                 .build();
+    }
+
+    @Test
+    void should_return_task_when_find_task_by_id_called() throws Exception {
+        int id = 123;
+        Mockito.when(taskService.findTaskById(id)).thenReturn(testTask);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/tasks/{id}",123))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+
+        Mockito.verify(taskService).findTaskById(123);
     }
 }
