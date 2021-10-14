d51f8d1837b46c4b3a4eb8f1c11dae908fae7f0c
Wei Wang <wei.wang2thoughtworks.com@Weis-MacBook-Pro.local>
2021-10-14 15:54:20 +0800

    [wangwei]feat:finished the create and find users in controler layer

--- src/main/java/com/example/backendbootcamp/controller/UserController.java
+++ src/main/java/com/example/backendbootcamp/controller/UserController.java
@@ -1,13 +1,30 @@
 package com.example.backendbootcamp.controller;
 
+import com.example.backendbootcamp.entity.User;
 import com.example.backendbootcamp.service.UserService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @RequestMapping(path = "/users", method = RequestMethod.POST)
+    public Long createUser(@RequestBody User user) {
+        userService.createUser(user);
+        return user.getId();
+    }
+
+    @RequestMapping(path = "/users/all", method = RequestMethod.GET)
+    public List<User> findAllUsers() {
+        return userService.findAllUsers();
     }
 }
