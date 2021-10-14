382dc10a4a58f5f9ef114ce60b744a76d6d3520b
Wei Wang <wei.wang2thoughtworks.com@Weis-MacBook-Pro.local>
2021-10-14 14:32:35 +0800

    [wangwei]feat:achieved the create user function in repository layer

--- src/main/java/com/example/backendbootcamp/repository/UserRepository.java
+++ src/main/java/com/example/backendbootcamp/repository/UserRepository.java
@@ -9,4 +9,9 @@ import java.util.List;
 @Repository
 public class UserRepository {
     private final List<User> userList = new ArrayList<>();
+
+    private Long createUser(User user) {
+        userList.add(user);
+        return user.getId();
+    }
 }
